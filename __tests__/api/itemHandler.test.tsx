a27ae6074169c4201c2640f5ef3ef6631324f9d5
Irene Sanchez <irene.iscc777@gmail.com>
2022-12-14 13:03:26 -0500

    item handler test

--- __tests__/api/itemHandler.test.tsx
+++ __tests__/api/itemHandler.test.tsx
@@ -19,16 +19,21 @@ describe("Add List API", () => {
     await mongoServer.stop();
   });
 
-//   test("api returns list object and status 200", async () => {
-//     const { req, res } = createMocks({
-//       method: "POST",
-//       body: { title: "Test" },
-//     });
+  test("api returns list object and status 200", async () => {
+    const { req, res } = createMocks({
+      method: "POST",
+      body: { title: "Test item" },
+    });
 
-//     await itemHandler(req, res);
-//     expect(res._getStatusCode()).toBe(200);
-//     expect(JSON.parse(res._getData())).toEqual(
-//       expect.objectContaining({ __v: 0, title: "Test" })
-//     );
+    await itemHandler(req, res);
+    expect(res._getStatusCode()).toBe(200);
+    expect(JSON.parse(res._getData()).item).toEqual(
+      expect.objectContaining({
+        title: "Test item",
+        completed: false,
+        isDeleted: false,
+        __v: 0,
+      })
+    );
   });
 });
